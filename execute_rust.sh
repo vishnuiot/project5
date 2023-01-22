006be672a5d75ee2203bd8f2f0cba9135c6f3bb8
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-22 22:08:26 +0100

    modification

---
+++ execute_rust.sh
@@ -0,0 +1,2 @@
+
+./gitupload.sh
---
+++ gitupload.sh
@@ -0,0 +1,6 @@
+# git init . // only for brand new projects
+git add .    # all files
+git commit -m "modification"
+#git remote add origin git@github.com:vishnuiot/project5.git
+git pull origin main
+git push origin main
