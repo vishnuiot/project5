cb633aff07e69a18ff895e9dad4ce181e12a099f
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-25 13:41:27 +0100

    modification

---
+++ rust_variables/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // allows x value to be changed
+    let mut x=5;
+    println!("The value of x is {}",x);  
+    // println!("Hello, world!");
+    x=50;
+    println!("The value of x is {}",x);
+}
