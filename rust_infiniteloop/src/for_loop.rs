6e43b666db24819a99b51c5ffe1609b705c57128
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-30 15:44:54 +0100

    modification

--- rust_infiniteloop/src/for_loop.rs
+++ rust_infiniteloop/src/for_loop.rs
@@ -6,4 +6,17 @@ fn main() {
     for i in 21..31 {
         println!("The number is {}", i);
     }
+
+    let numbers = 70..91;
+    for i in numbers {
+        println!("The number is {}", i);
+    }
+
+    let animals=vec!["cat","rhino","Elephant"];
+
+    for (index,a) in animals.iter().enumerate(){
+        println!("The index is {} and the animal is {}",index,a)
+    }
+
+
 }
