d0fb292ce7649b75d143ceaa0c7ec4df56c350fa
vishnuiot <vishnuprasad.iit@gmail.com>
2023-01-30 15:38:59 +0100

    modification

--- rust_infiniteloop/src/for_loop.rs
+++ rust_infiniteloop/src/for_loop.rs
@@ -1,6 +1,9 @@
-fn main()
-{
-    for i in 1.. 11{
-        println!({},i);
+fn main() {
+    for i in 1..11 {
+        println!("The number is {}", i);
+    }
+
+    for i in 21..31 {
+        println!("The number is {}", i);
     }
 }
